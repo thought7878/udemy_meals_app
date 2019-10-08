1021e64fc28d02140c29d994d15c634b6e7ffa49
Calm Andy <thought7878@qq.com>
2019-10-08 10:43:19 +0800

    154. Starting With The App - Adding Base Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:udemy_meals_app/screans/category_screen.dart';
+import './screens/category_screen.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CategoryScreen'),
+        elevation: 0.0,
+      ),
+      body: GridView(
+        children: <Widget>[],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
