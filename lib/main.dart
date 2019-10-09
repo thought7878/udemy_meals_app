b4dc805d4edd62c0fd0c84396013f29d1147d56f
Calm Andy <thought7878@qq.com>
2019-10-09 18:46:29 +0800

    172. Adding Links to the Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:udemy_meals_app/screens/tabs_screen.dart';
-
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_screen.dart';
+import './screens/filters_screen.dart';
+import './screens/tabs_screen.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -41,6 +41,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (context) => MealDetailScreen(),
+        FiltersScreen.routeName: (context) => FiltersScreen(),
       },
       // onGenerateRoute: (settings){
       //   return MaterialPageRoute(builder: (context){
---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const String routeName = '/filters';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FiltersScreen'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
